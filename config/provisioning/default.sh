3fa56ad25274c42c4527b9b90c0fb49e116d9155
豚痕 <108185826+TunHen@users.noreply.github.com>
2024-08-08 00:27:57 +0800

    Update default.sh

--- config/provisioning/default.sh
+++ config/provisioning/default.sh
@@ -41,7 +41,7 @@ LORA_MODELS=(
 
 VAE_MODELS=(
     #"https://huggingface.co/stabilityai/sd-vae-ft-ema-original/resolve/main/vae-ft-ema-560000-ema-pruned.safetensors"
-    "https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors"
+    #"https://huggingface.co/stabilityai/sd-vae-ft-mse-original/resolve/main/vae-ft-mse-840000-ema-pruned.safetensors"
     "https://huggingface.co/stabilityai/sdxl-vae/resolve/main/sdxl_vae.safetensors"
 )
 
